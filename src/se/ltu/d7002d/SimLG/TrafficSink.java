d5cf25825b43a48dc02fa8b944e20fcefb78acf6
BAMBANZA Jean Paul <jeanpok8@gmail.com>
2015-02-14 13:27:38 +0100

    Update TrafficSink.java

--- src/se/ltu/d7002d/SimLG/TrafficSink.java
+++ src/se/ltu/d7002d/SimLG/TrafficSink.java
@@ -1,107 +1,55 @@
 package se.ltu.d7002d.SimLG;
 
 /**
- * This class implements a traffic Sink that registers incoming packets and
- * prepares statistics to be printed, it contains
- * 
- * @author Bambanza
- *
+ * This class implements a traffic Sink that registers incoming & receiving
+ * messages and prepares statistics to be printed, it contains different
+ * functions to calculate the number of sent messages, received messages,dropped
+ * messages in percentage,elapsed time, and received messages per millisecond.
  */
 
-public class TrafficSink extends Node {
+public class TrafficSink {
 
-	private int sentPackets = 0;
-	private int receivedPackets = 0;
-	private int time = 0;
-	private TrafficSink myTrafficSink;
+	private int RcvdMsgCounter=0;
+	private int sentMessages =0;
+	private float averagePerms=0.0;
+	private int timeBetweenSending=10;// time between messages
 
-	/**
-	 * Creates an object with an number of sent packets by the sending node,time
-	 * and the number received packets.
-	 * 
-	 * @param sentPackets
-	 *            Represents the number of sent packets.
-	 * @param receivedPackets
-	 *            Represents the number of received packets.
-	 * @param time
-	 *            Represents time delay.
-	 * 
-	 */
-	public TrafficSink(int network, int node, int sentPackets,
-			int receivedPackets, int time) {
-		super(network, node);
-		this.sentPackets = sentPackets;
-		this.receivedPackets = receivedPackets;
-		myTrafficSink = new TrafficSink(network, node, sentPackets,
-				sentPackets, time);
+	public int countMessage(){
+		RcvdMsgCounter+=1;
+		return RcvdMsgCounter=0;
 	}
 
-	public int getReceivedPackets(int receivedPackets) {
-		return receivedPackets;
+	public void getsentMsgs(){
+
+		sentMessages += 1;
 	}
 
-	public int getTime(int time) {
+	public float droppedMsgs(){
 
-		time = _timeBetweenSending;
-
-		return time;
+		return (sentMessages - RcvdMsgCounter);
 	}
 
-	public int getSentPackets(int sentPackets) {
+	public int getTime(){
+		timeBetweenSending += 10;
+		return timeBetweenSending;
+	}
 
-		return sentPackets;
+	public double getAverage(){
+		averagePerms=countMessage()/getTime();
+		return averagePerms;
 	}
 
 	public void printStatistics() {
-		System.out.println("Number of sent packets =" + sentPackets);
-		System.out.println("received packets at" + time);
-		System.out.println("Number of receved packets =" + receivedPackets);
+
+		System.out.println("                                   ");
+		System.out.println("Summary Statistics  ");
+		System.out.println("                                   ");
+		System.out.println("Received messages :" + RcvdMsgCounter);
+		System.out.println("Sent messages : " + sentMessages);
+		System.out.println("Dropped messages:" + droppedMsgs() / 100 + "%");
+		System.out.println("Elapsed time: " + getTime() + "μs");
+		System.out.println("Avg.receivedMsg/μs:" + getAverage());
 
 	}
 
-	public void recv(SimEnt src, Event ev) {
-		if (ev instanceof TimerEvent) {
-			if (_stopSendingAfter > _sentmsg) {
-				_sentmsg++;
-				NetworkAddr source = new NetworkAddr(_id.networkId(),
-						_id.nodeId());
-				NetworkAddr destination = new NetworkAddr(_toNetwork, _toHost);
-				send(_peer, new Message(_id, destination, _seq), 0);
-				send(this, new TimerEvent(), _timeBetweenSending);
-				Utils.logMessage(source, destination,
-						"Node " + source.dotAddress()
-								+ " sent message with seq: " + _seq);
-				_seq++;
-
-			}
-		}
-		if (ev instanceof Message) {
-			receivedPackets++;
-			myTrafficSink.getReceivedPackets(receivedPackets);
-			Utils.logMessage(
-					((Message) ev).source(),
-					((Message) ev).destination(),
-					"Node " + _id.networkId() + "." + _id.nodeId()
-							+ " receives message with seq: "
-							+ ((Message) ev).se
-							q());
-
-		}
-	}
-
-	/**
-	 * This method is called upon that an event destined for this node triggers.
-	 * 
-	 */
-	public void StartSending(int network, int node, int number,
-			int timeInterval, int startSeq) {
-		_stopSendingAfter = number;
-		_timeBetweenSending = timeInterval;
-		_toNetwork = network;
-		_toHost = node;
-		_seq = startSeq;
-		sentPackets++;
-		myTrafficSink.getSentPackets(sentPackets);
-		send(this, new TimerEvent(), 0);
-	}
 }
